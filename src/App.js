a6d97b5c5a86825d032285bb3d400ceb789a75ea
Zain <zain.frayha@gmail.com>
2024-06-14 18:33:48 +0200

    commiting to check if seperated.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import ProfilePage from './pages/ProfilePage';
-import FoodListPage from './pages/FoodListPage';
-import CartProvider from './contexts/CartContext';
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import ProfilePage from './Pages/ProfilePage';
+import FoodListPage from './Pages/FoodListPage';
+
+
 
 const App = () => (
   <CartProvider>
